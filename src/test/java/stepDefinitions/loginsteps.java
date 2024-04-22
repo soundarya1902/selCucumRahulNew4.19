874c0fef1c8b1071849bfe7f35026373881bcde7
soundarya <soundarya.arikati@miele.com>
2024-04-22 10:32:55 +0200

    added webdrivermanwager

--- src/test/java/stepDefinitions/loginsteps.java
+++ src/test/java/stepDefinitions/loginsteps.java
@@ -3,11 +3,14 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 
 public class loginsteps {
+    private  static WebDriver driver=null;
     @Given("user is on landingpage")
     public void user_is_on_landingpage() {
-
+        driver = WebDriverManager.chromedriver().create();
     }
     @When("user login with valid credentails")
     public void user_login_with_valid_credentails() {
